ef423ef8353e1759112e3dc43d41895d9da3c25d
Christopher Wood <christopherwood07@gmail.com>
2015-09-01 12:53:21 -0700

    removed GUI

--- client.go
+++ client.go
@@ -7,7 +7,6 @@ import (
     "os"
     "fmt"
     "strings"
-    "github.com/jroimartin/gocui"
 )
 
 type Command struct {
@@ -17,7 +16,7 @@ type Command struct {
 
 func handleCommand(command Command, connection net.Conn) (bool) {
     if command.command == "quit" {
-        return false 
+        return false
     } else {
         fmt.Println(command.command)
         return true
@@ -30,14 +29,14 @@ func parseCommandString(commandString string) (Command) {
     if len(stringFields) > 0 {
         command.command = stringFields[0]
         if len(stringFields) > 1 {
-            command.parameters = stringFields[1:]    
+            command.parameters = stringFields[1:]
         } else{
             command.parameters = nil
         }
     } else {
         // error?
     }
-    
+
     return command
 }
 
@@ -115,12 +114,12 @@ func ircHandler(channelFromServer chan string, channelToServer chan string) {
     // if len(stringReply) > 0 {
     //     // fmt.Println(stringReply)
     //     // view.Clear()
-    //     fmt.Fprintln(view, stringReply) 
+    //     fmt.Fprintln(view, stringReply)
     //     gui.Flush()
     // }
 }
 
-func runSession(gui *gocui.Gui, serverAddress string) (error) {
+func runSession(serverAddress string) (error) {
     connection, err := net.Dial("tcp", serverAddress)
     if err != nil {
         return err;
@@ -135,180 +134,6 @@ func runSession(gui *gocui.Gui, serverAddress string) (error) {
 
     <-killChannel // block until killed
     stopSession(connection)
-
-    return nil
-}
-
-func nextView(g *gocui.Gui, v *gocui.View) (error) {
-    if v == nil || v.Name() == "rightside" {
-        return g.SetCurrentView("main")
-    } else if v == nil || v.Name() == "main" {
-        return g.SetCurrentView("input")
-    } else {
-        return g.SetCurrentView("rightside")
-    }
-}
-
-func cursorDown(g *gocui.Gui, v *gocui.View) (error) {
-    if v != nil {
-        cx, cy := v.Cursor()
-        if err := v.SetCursor(cx, cy+1); err != nil {
-            ox, oy := v.Origin()
-            if err := v.SetOrigin(ox, oy+1); err != nil {
-                return err
-            }
-        }
-    }
-    return nil
-}
-
-func cursorUp(g *gocui.Gui, v *gocui.View) (error) {
-    if v != nil {
-        ox, oy := v.Origin()
-        cx, cy := v.Cursor()
-        if err := v.SetCursor(cx, cy-1); err != nil && oy > 0 {
-            if err := v.SetOrigin(ox, oy-1); err != nil {
-                return err
-            }
-        }
-    }
-    return nil
-}
-
-func cursorLeft(g *gocui.Gui, v *gocui.View) (error) {
-    if v != nil {
-        ox, oy := v.Origin()
-        cx, cy := v.Cursor()
-        if err := v.SetCursor(cx-1, cy); err != nil && ox > 0 {
-            if err := v.SetOrigin(ox-1, oy); err != nil {
-                return err
-            }
-        }
-    }
-    return nil
-}
-
-func cursorRight(g *gocui.Gui, v *gocui.View) (error) {
-    if v != nil {
-        cx, cy := v.Cursor()
-        if err := v.SetCursor(cx+1, cy); err != nil {
-            ox, oy := v.Origin()
-            if err := v.SetOrigin(ox+1, oy); err != nil {
-                return err
-            }
-        }
-    }
-    return nil
-}
-
-func getLine(g *gocui.Gui, v *gocui.View) error {
-    var l string
-    var err error
-
-    _, cy := v.Cursor()
-    if l, err = v.Line(cy); err != nil {
-        l = ""
-    }
-
-    maxX, maxY := g.Size()
-    if v, err := g.SetView("msg", maxX/2-30, maxY/2, maxX/2+30, maxY/2+2); err != nil {
-        if err != gocui.ErrorUnkView {
-            return err
-        }
-        fmt.Fprintln(v, l)
-        if err := g.SetCurrentView("msg"); err != nil {
-            return err
-        }
-    }
-    return nil
-}
-
-func delMsg(g *gocui.Gui, v *gocui.View) error {
-    if err := g.DeleteView("msg"); err != nil {
-        return err
-    }
-    if err := g.SetCurrentView("side"); err != nil {
-        return err
-    }
-    return nil
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-    return gocui.Quit
-}
-
-func keybindings(g *gocui.Gui) error {
-    if err := g.SetKeybinding("rightside", gocui.KeyCtrlSpace, gocui.ModNone, nextView); err != nil {
-        return err
-    }
-    if err := g.SetKeybinding("main", gocui.KeyCtrlSpace, gocui.ModNone, nextView); err != nil {
-        return err
-    }
-    if err := g.SetKeybinding("", gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
-        return err
-    }
-    if err := g.SetKeybinding("", gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
-        return err
-    }
-    if err := g.SetKeybinding("", gocui.KeyArrowLeft, gocui.ModNone, cursorLeft); err != nil {
-        return err
-    }
-    if err := g.SetKeybinding("", gocui.KeyArrowRight, gocui.ModNone, cursorRight); err != nil {
-        return err
-    }
-    if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-        return err
-    }
-    // if err := g.SetKeybinding("side", gocui.KeyEnter, gocui.ModNone, getLine); err != nil {
-    //     return err
-    // }
-    if err := g.SetKeybinding("input", gocui.KeyEnter, gocui.ModNone, delMsg); err != nil {
-        return err
-    }
-
-    return nil
-}
-
-func layout(g *gocui.Gui) error {
-    maxX, maxY := g.Size()
-
-    // right side (channel list) 
-    if v, err := g.SetView("rightside", maxX - 30, 0, maxX - 1, maxY - 3); err != nil {
-        if err != gocui.ErrorUnkView {
-            return err
-        }
-        v.Highlight = true
-        fmt.Fprintln(v, "Channel list")
-    }
-
-    // input field 
-    if v, err := g.SetView("input", 0, maxY - 3, maxX - 1, maxY - 1); err != nil {
-        if err != gocui.ErrorUnkView {
-            fmt.Println("Error: could not set `main` view")
-            return err
-        }
-
-        v.Editable = true
-        v.Wrap = false
-        v.Highlight = true
-    }
-
-    // main side 
-    if v, err := g.SetView("main", 10, 0, maxX - 20, maxY - 3); err != nil {
-        if err != gocui.ErrorUnkView {
-            fmt.Println("Error: could not set `main` view")
-            return err
-        }
-
-        v.Editable = false
-        v.Wrap = true
-        fmt.Fprintln(v, "Here we go...")
-
-        if err := g.SetCurrentView("main"); err != nil {
-            fmt.Println("couldn't set view to main")
-            return err
-        }
-    }
 
     return nil
 }
@@ -331,34 +156,9 @@ func main() {
 
     fmt.Fprintln(os.Stderr, "Got the arguments, setting up the GUI")
 
-    // gui := gocui.NewGui()
-    // fmt.Fprintln(os.Stderr, "Initializing.")
-    // err = gui.Init();
-    // fmt.Fprintln(os.Stderr, "Checking for failure.")
-    // if err != nil {
-    //     fmt.Fprintln(os.Stderr, "Error during initialization: %s", err)
-    //     log.Panicln(err)
-    // }
-    // defer gui.Close()
-
-    // fmt.Fprintln(os.Stderr, "Setup the GUI")
-
-    // gui.SetLayout(layout)
-    // if err := keybindings(gui); err != nil {
-    //     log.Panicln(err)
-    // }
-    // gui.SelBgColor = gocui.ColorGreen
-    // gui.SelFgColor = gocui.ColorBlack
-    // gui.ShowCursor = true
-
-    err = runSession(nil, args[0])
-    // if err != nil {
-    //     fmt.Fprintln(os.Stderr, "Failed.")
-    //     fmt.Fprintln(os.Stderr, "Error: " + string(err.Error()))
-    // } else {
-    //     err = gui.MainLoop()
-    //     if err != nil && err != gocui.Quit {
-    //         log.Panicln(err)
-    //     }
-    // }
+    err = runSession(args[0])
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "Failed.")
+        fmt.Fprintln(os.Stderr, "Error: " + string(err.Error()))
+    }
 }
