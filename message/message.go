b7d0911ed906cc3d76c78e9dc572372c70edc82c
Christopher Wood <christopherwood07@gmail.com>
2016-06-05 14:04:58 -0700

    Message parsing

--- message/message.go
+++ message/message.go
@@ -2,6 +2,7 @@ package message
 
 import (
     // "time"
+    "fmt"
 )
 
 type Message struct {
@@ -10,14 +11,28 @@ type Message struct {
     Parameters []string
 }
 
-func parseMessageParameters(message []byte) []string {
-    list := make([]string, 1)
-
-    return list
+type MessageError struct {
+    message string
 }
 
-func parseMessageCommand(message []byte) string {
-    return ""
+func (e MessageError) Error() string {
+    return e.message
+}
+
+func parseMessageParameters(message []byte) ([]string, error) {
+    list := make([]string, 1)
+
+    return list, nil
+}
+
+func parseMessageCommand(message []byte) (string, error) {
+    length := len(message)
+    for i := 0; i < length; i++ {
+        if message[i] == ' ' {
+            return string(message[0:i]), nil
+        }
+    }
+    return "", MessageError{"Could not parse a command"}
 }
 
 func parseMessagePrefix(message []byte) string {
@@ -31,8 +46,19 @@ func parseMessageAsBytes(message []byte) *Message {
         msg.Prefix = parseMessagePrefix(message[1:])
     }
 
-    msg.Command = parseMessageCommand(message)
-    msg.Parameters = parseMessageParameters(message[len(msg.Command):])
+    command, commandError := parseMessageCommand(message)
+    if commandError == nil {
+        msg.Command = command
+    } else {
+        fmt.Println("Error parsing command")
+    }
+
+    parameters, paramError := parseMessageParameters(message[len(msg.Command):])
+    if paramError == nil {
+        msg.Parameters = parameters
+    } else {
+        fmt.Println("Error parsing parameters")
+    }
 
     return msg
 }
